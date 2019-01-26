4618894fcb2bf5ad1a0c4afa176cc03a4aa01715
Duraimurugan Govindaraj <durai145@gmail.com>
2019-01-26 12:09:58 -0500

    fixed

--- app.js
+++ app.js
@@ -151,10 +151,10 @@ app.post("/api/dashboard/resetPassword", function (req, res) {
             state = container.state();
             if (state.status == "Running") {
                 console.log("in R:002 ###");
-                const cmd = ["echo \"This is test\""];
+                const cmd = "echo \"This is test\"";
                 console.log("in R:004 ###:" + cmd);
 
-                container.run(cmd, function (err, stdOut, stdErr) {
+                container.run(["pwd"], function (err, stdOut, stdErr) {
                     console.log("in R:004.001 ###:" + err + "|" + stdErr + "|" + stdOut);
                     if (err) {
                         res.json({
